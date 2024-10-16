4a4ca9872dca55285ef192216fd6c6eb0e009a59
chimitslucie <chimitslucie@gmail.com>
2024-10-16 18:19:02 +0200

    finish

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,33 @@
+import { useForm } from 'react-hook-form';
+
 function Form() {
+
+    const {
+        register,
+        handleSubmit = e => {
+            e.preventDefault();
+        },
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            email: "",
+        },
+    });
+
     return (
-        <form className="form">
-            <div className="formContent">
-                <input type="email" name="email" id="email" className="formContentEmail" placeholder="Email address" />
+        <form className="form" onSubmit={handleSubmit((d) => console.log(d))} noValidate>
+            <div className={errors.email ? 'formContent error' : 'formContent'}>
+                <input type="email" name="email" id="email"
+                    className="formContentEmail"
+                    placeholder="Email address"
+                    {...register('email', {
+                        validate: {
+                            matchPattern: (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || "Oops! Please check your email",
+                        },
+                    })} />
                 <button type="submit" className="formContentBtn" >Request Access</button>
+
+                {errors.email?.message && (<p className='formMessageError'>{errors.email.message}</p>)}
             </div>
         </form>
     );
